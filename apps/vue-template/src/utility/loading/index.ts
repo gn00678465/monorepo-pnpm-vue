07900ec7c5bbee15d8f6f184ed6414fe174bcffc
madao <gn00678465@gmail.com>
2024-06-04 17:08:30 +0800

    refactor: 💡 重構 loading

--- apps/vue-template/src/utility/loading/index.ts
+++ apps/vue-template/src/utility/loading/index.ts
@@ -1,23 +1,51 @@
-export function setupLoading() {
-  const loading = document.querySelector('#loading') as HTMLElement;
-
-  const cubeLoading = document.createElement('cube-loading');
-
-  cubeLoading.title = 'Loading...';
-
-  loading?.appendChild(cubeLoading);
-
-  return loading;
+interface LoadingOptions {
+  title?: string;
+  subtitle?: string;
+  once?: boolean;
 }
 
-export function removeLoading(loading?: HTMLElement) {
-  return new Promise<void>((resolve, reject) => {
-    (loading &&
-      setTimeout(() => {
-        loading.style.opacity = '0.02';
-        setTimeout(() => document.body.removeChild(loading), 600);
-        resolve();
-      })) ||
-      reject();
-  });
+export class Loading {
+  private loading: HTMLElement | null;
+  private loadingNode: HTMLElement;
+
+  private initialFlagKey = 'initialFlag';
+  private once = false;
+  constructor(options: LoadingOptions = {}) {
+    const { title, subtitle, once = false } = options;
+    this.loading = document.querySelector('#loading');
+    this.loadingNode = document.createElement('cube-loading');
+    this.loadingNode.title = title ? title : 'Loading...';
+    if (subtitle && 'description' in this.loadingNode) {
+      this.loadingNode.description = subtitle;
+    }
+    this.once = once;
+  }
+
+  get flag(): string | null {
+    return window.sessionStorage.getItem(this.initialFlagKey);
+  }
+
+  set flag(v: string) {
+    window.sessionStorage.setItem(this.initialFlagKey, v);
+  }
+
+  start(): void {
+    if (this.once && this.flag === 'y') return;
+    this.loading?.appendChild(this.loadingNode);
+  }
+
+  stop(): void {
+    if (this.once) {
+      this.flag = 'y';
+    }
+    setTimeout(() => {
+      if (this.loading) {
+        this.loading.style.opacity = '0.02';
+      }
+      setTimeout(
+        () => this.loading && document.body.removeChild(this.loading),
+        600
+      );
+    });
+  }
 }
